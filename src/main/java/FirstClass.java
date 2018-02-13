88c2b81d1f1f98f117b4dd5c14649a5020f53383
ket163 <hovmfa@gmail.com>
2018-02-13 23:57:00 +0300

    second commit

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -2,5 +2,13 @@ public class FirstClass {
 
     public static void main(String[] args) {
         System.out.println("Initial commit");
+
+        int a = 1;
+        int b = 2;
+        int sum = a+b;
+        System.out.println(sum);
+        System.out.println(a);
+        System.out.println(b);
+
     }
 }
